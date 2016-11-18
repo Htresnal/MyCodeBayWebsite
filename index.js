2514acf5c3cc5f23b4683dc3a1b994466a18f2d6
Htresnal <sieggallery@yandex.ru>
2016-11-18 21:54:53 +0300

    C:/Program Files (x86)/Git/GET fix

--- index.js
+++ index.js
@@ -13,6 +13,22 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+app.get('/index', function(request, response) {
+  response.redirect('/');
+});
+
+app.get('/about', function(request, response) {
+  response.render('pages/about');
+});
+
+app.get('/games', function(request, response) {
+  response.render('pages/games');
+});
+
+app.get('/media', function(request, response) {
+  response.render('pages/media');
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
