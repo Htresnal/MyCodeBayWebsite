114197dd3cee06ec14c0bc7a8a8961bd52f576ee
Htresnal <sieggallery@yandex.ru>
2017-04-28 23:59:48 +0300

    index text change. Enriched portfolio text

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 var express = require('express');
+const pg = require('pg');
+const connectionString = process.env.DATABASE_URL || 'postgres://bemaovcelglemi:b0f7f122431317ce65931e2bc327fa6e86fcdb614358264932b7e8f297fd3778@ec2-23-21-96-70.compute-1.amazonaws.com:5432/d8uqn1mr35dekr';
+
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/node_modules/angular'));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
@@ -27,6 +31,10 @@ app.get('/programs', function(request, response) {
 
 app.get('/links', function(request, response) {
   response.render('pages/links');
+});
+
+app.get('/db', function(request, response) {
+  response.render('pages/db');
 });
 
 app.listen(app.get('port'), function() {
